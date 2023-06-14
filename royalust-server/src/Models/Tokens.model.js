8593b407abae4a755b90659f42e4ca03b7e59e4f
Mihir Sharma <mihirsharmaofficial@gmail.com>
2023-06-14 18:41:05 +0530

    false commit

--- royalust-server/src/Models/Tokens.model.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const TokensSchema = new Schema({
-  token: {type: String, unique: true, required: true}
-});
-let TokensModel = mongoose.model('Tokens', TokensSchema);
-module.exports = TokensModel;
